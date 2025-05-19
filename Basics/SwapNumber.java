53732de0e1198c160611c284db8f860102f1027d
subashini2203 <subashininaveen2203@gmail.com>
2025-05-19 21:25:01 +0530

    swap two variable

---
+++ Basics/SwapNumber.java
@@ -0,0 +1,14 @@
+public class SwapNumber {
+    public static void main(String[] args){
+int a=15;
+int b=10;
+System.out.println("Before swap:a="+a+",b="+b);
+int temp=a;
+a=b;
+b=temp;
+System.out.println("After swap:a="+a+",b="+b);
+
+
+
+    }
+}
