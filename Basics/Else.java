9e87caf3f8d33e5fc5856d76c6a05c031b3b7107
subashini2203 <subashininaveen2203@gmail.com>
2025-05-20 15:38:32 +0530

    Even number

---
+++ Basics/Else.java
@@ -0,0 +1,11 @@
+public class Else{
+public static void main(String[] args){
+    int number=10;
+    if(number%2==0){
+        System.out.println("Even number is:"+number);
+    }
+    else{
+        System.out.println("odd number:"+number);
+    }
+}
+}
