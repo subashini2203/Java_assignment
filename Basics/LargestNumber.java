ad191d195ff75f2fa1fe93dfa8a2cd53bf06f2ba
subashini2203 <subashininaveen2203@gmail.com>
2025-05-24 18:04:47 +0530

    LargestNumber

---
+++ Basics/LargestNumber.java
@@ -0,0 +1,28 @@
+public class LargestNumber{
+   static int a=60;
+   static int b=45;
+   static int c=70;
+    public static void main(String[] args){
+        if(a>b){
+            if(a>c){
+                System.out.println("largest number:"+a);
+            }else{
+                System.out.println("largest number:"+c);
+            }
+        }else{
+                if(b>c){
+                    System.out.println("largest number:"+b);
+                }else{
+                    System.out.println("largest number:"+c);
+                }
+            }
+        
+    }
+
+
+    }
+
+        
+        
+    
+
