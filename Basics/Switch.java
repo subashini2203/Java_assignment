40d96bb0a43e4e8f124c3a7741bb8ed7216c16af
subashini2203 <subashininaveen2203@gmail.com>
2025-05-26 17:23:53 +0530

    Switch

---
+++ Basics/Switch.java
@@ -0,0 +1,24 @@
+public class Switch{
+    public static void main(String[] args){
+        char input='i';
+        switch(input){
+            case 'a':
+        
+            case 'e':
+        
+            case 'i':
+          
+            case 'o':
+           
+            case 'u':
+             System.out.println(input+"is a vowel");
+            break;
+           
+            default:
+            System.out.println("consonant");
+
+        }
+
+        
+    }
+}
