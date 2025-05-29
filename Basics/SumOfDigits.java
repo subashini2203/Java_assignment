657056da6f71e8be38e660d74f31416640d49667
subashini2203 <subashininaveen2203@gmail.com>
2025-05-29 16:24:40 +0530

    SumOfDigits

---
+++ Basics/SumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt(); 
+        int sum = 0; 
+        while (number != 0) {
+            int digit = number % 10;    
+            sum = sum + digit;          
+            number = number / 10;       
+        }
+
+        // Print the result
+        System.out.println("Sum of digits: " + sum);
+    }
+}
