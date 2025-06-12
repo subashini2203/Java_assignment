714f1160601485b60785a0a7b705b8be84e674c1
subashini2203 <subashininaveen2203@gmail.com>
2025-06-12 18:20:23 +0530

    polymorphism

---
+++ Java_oops_Assignment/Polymorphism.java
@@ -0,0 +1,24 @@
+class shape{
+    void draw(){
+        System.out.println("drawing a shape");
+    }
+}
+class circle extends shape{
+    void draw(){
+        System.out.println("drawing a circle");
+    }
+}
+class square extends shape{
+    void draw(){
+        System.out.println("drawing a square");
+    }
+}
+public class Polymorphism {
+    public static void main(String[] args){
+        shape c=new circle();
+        shape s=new square();
+        c.draw();
+        s.draw();
+    }
+    
+}
