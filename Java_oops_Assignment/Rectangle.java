20a81d45e44ae2c346ea2f7cd118fd20f11d20fa
subashini2203 <subashininaveen2203@gmail.com>
2025-06-14 18:57:55 +0530

    addednewlines

--- Java_oops_Assignment/Rectangle.java
+++ Java_oops_Assignment/Rectangle.java
@@ -1,15 +1,39 @@
 public class Rectangle {
     int width;
     int length;
+    // Default constructor
+    Rectangle() {
+        width = 1;
+        length = 1;
+    }
     Rectangle(int num1,int num2){
         width=num1;
         length=num2;
  }
 
+ public double calculatearea(){
+    double area=width*length;
+    return area;
+
+ }
+
 public static void main(String[] args){
-    Rectangle r=new Rectangle(1,1);
-    System.out.println(r.width);
-    System.out.println(r.length);
+
+    Rectangle r1=new Rectangle();
+    
+    double r1area1=r1.calculatearea();
+    System.out.println("Area of rectangle r1: " + r1area1);
+
+    
+
+
+
+    Rectangle r=new Rectangle(5,10);
+   
+    double rarea=r.calculatearea();
+    System.out.println("Area of rectangle r:"+ rarea);
+
+    
 }
 
 }
