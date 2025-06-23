569b8f885e31c0e458f4b45e4fb510246f8cc05c
subashini2203 <subashininaveen2203@gmail.com>
2025-06-23 17:27:31 +0530

    foomethod

---
+++ ExceptionHandling/Foo.java
@@ -0,0 +1,20 @@
+public class Foo {
+    // This method throws an exception
+    public static void foo() throws Exception{
+        System.out.println("foo method called");
+       
+        int result=10/0; // ArithmeticException
+    }
+    public static void main(String[] args){
+        try{
+            //  calling foo method
+            foo();
+        }catch(Exception e){
+            System.out.println("Exception caught:"+e.getMessage());
+        }
+
+        }
+    }
+
+    
+
