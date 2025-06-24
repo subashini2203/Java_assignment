4849115aa23c1469c49ddb4c9b3c165a4d07413e
subashini2203 <subashininaveen2203@gmail.com>
2025-06-24 19:09:05 +0530

    studentdetails

--- Java_oops_Assignment/Student.java
+++ Java_oops_Assignment/Student.java
@@ -1,21 +1,23 @@
 public class Student {
     String name;
     int Rollno;
-   void display1(){
+   void display(){
     System.out.println("Student1 details:");
-    System.out.println("Name: " + "John");
-    System.out.println("Rollno:"+78);
+    System.out.println("Name: " + this.name);
+    System.out.println("Rollno:"+this.Rollno);
 }
-   void display2(){
-    System.out.println("Student2 details:");
-    System.out.println("Name:" + "hello");
-    System.out.println("Rollno:"+89);
-   }
+
    public static void main(String[] args){
    Student obj1=new Student();
    Student obj2=new Student();
-   obj1.display1();
-   obj2.display2();
-  
+
+   obj1.name="john";
+    obj1.Rollno=101;
+
+    obj2.name="jane";
+    obj2.Rollno=102;
+    
+   obj1.display();
+   obj2.display();
    }
 }
