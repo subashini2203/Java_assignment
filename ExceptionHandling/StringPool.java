fee28bd2f212ac583b0494b84c4fd61c565d0795
subashini2203 <subashininaveen2203@gmail.com>
2025-07-01 15:09:32 +0530

    Both string are same

---
+++ ExceptionHandling/StringPool.java
@@ -0,0 +1,9 @@
+public class StringPool {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Java";
+        System.out.println("str1 == str2: " + (str1 == str2));
+        System.out.println("s1.equals(s2):"+(str1.equals(str2))); 
+    }
+}
+        
