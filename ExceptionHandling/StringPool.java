4a0182fa0b7baf186969a738de75156cc869f274
subashini2203 <subashininaveen2203@gmail.com>
2025-07-01 15:03:04 +0530

    string constant pool

--- ExceptionHandling/StringPool.java
+++
@@ -1,9 +0,0 @@
-public class StringPool {
-    public static void main(String[] args) {
-        String str1 = "Java";
-        String str2 = "Java";
-        System.out.println("str1 == str2: " + (str1 == str2));
-        System.out.println("s1.equals(s2):"+(str1.equals(str2))); 
-    }
-}
-        
